b0d1711672b7e220869fa56298001c3645ba6be8
Leouarz <ghalielouarzazi@hotmail.com>
2021-09-08 15:13:49 +0200

    add condition when serieId = 0 and timestamp burn must be null

--- packages/query/src/graphql/plugins/PgDistinct.ts
+++ packages/query/src/graphql/plugins/PgDistinct.ts
@@ -17,21 +17,33 @@ export const PgDistinct =  makeExtendSchemaPlugin((build) => {
             const owner = sql.value(args.owner)
             return resolveInfo.graphile.selectGraphQLResultFromTable(
                 sql.fragment`(
-                  SELECT DISTINCT ON (serie_id) nft_entities.* 
-                  FROM public.nft_entities
-                  ${listed.value !== undefined || owner.value !== undefined ? sql.fragment` WHERE ` : sql.fragment``}
-                  ${listed.value === 1 ? sql.fragment`listed=1` : sql.fragment``}
-                  ${listed.value === 0 ? sql.fragment`listed=0` : sql.fragment``}
-                  ${owner.value !== undefined ? 
-                    listed.value !== undefined ?
-                    sql.fragment` and owner=${owner}`
-                    :
-                      sql.fragment`owner=${owner}`
-                  :
-                    sql.fragment``
-                  }
+                  ${sql.fragment`(
+                    SELECT DISTINCT ON (serie_id) nft_entities.* 
+                    FROM public.nft_entities
+                    WHERE timestamp_burn IS NULL
+                    AND serie_id <> '0'
+                    ${listed.value === 1 ? sql.fragment` AND listed=1` : sql.fragment``}
+                    ${listed.value === 0 ? sql.fragment` AND listed=0` : sql.fragment``}
+                    ${owner.value !== undefined ? sql.fragment` AND owner=${owner}` : sql.fragment``}
+                    ${listed.value === undefined ? sql.fragment` ORDER BY serie_id, listed desc` : sql.fragment``}
+                  )`}
+
+                  ${sql.fragment`UNION`}
+
+                  ${sql.fragment`(
+                    SELECT nft_entities.* 
+                    FROM public.nft_entities
+                    WHERE timestamp_burn IS NULL
+                    AND serie_id = '0'
+                    ${listed.value === 1 ? sql.fragment` AND listed=1` : sql.fragment``}
+                    ${listed.value === 0 ? sql.fragment` AND listed=0` : sql.fragment``}
+                    ${owner.value !== undefined ? sql.fragment` AND owner=${owner}` : sql.fragment``}
+                    ${listed.value === undefined ? sql.fragment` ORDER BY serie_id, listed desc` : sql.fragment``}
+                  )`}
+
                   ${listed.value === undefined ? sql.fragment` ORDER BY serie_id, listed desc` : sql.fragment``}
-                )`,
+                )`
+                ,
                 () => {}
             )
         },
